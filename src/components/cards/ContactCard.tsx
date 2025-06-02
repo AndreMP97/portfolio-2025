71de1f339839ed05476b31b460c8751a3b1a9ac4
André Pacheco <andre.m.pacheco.97@gmail.com>
2025-06-02 23:14:20 +0100

    fix: fixed ContactCard

--- src/components/cards/ContactCard.tsx
+++ src/components/cards/ContactCard.tsx
@@ -25,23 +25,21 @@ export const ContactCard: React.FC<TContactCardProps> = ({
   title,
 }) => {
   return (
-    <div className="mx-auto flex flex-col gap-8">
-      <div className="border-space-blue bg-midnight-navy hover:border-aqua-mint rounded-lg border p-8 shadow-xl transition-colors duration-300">
-        <h2 className="mb-6 text-center text-3xl leading-tight font-bold tracking-tight text-white">
-          {title}
-        </h2>
-        <div className="flex flex-col items-center gap-6 text-center">
-          <p className="text-lavender-blue max-w-2xl text-lg leading-relaxed font-normal whitespace-break-spaces">
-            {description}
-          </p>
-          {buttons && (
-            <div className="mt-4 flex flex-col gap-6 md:flex-row sm:items-center sm:gap-8">
-              {buttons.map((button) => (
-                <ButtonLink {...button} />
-              ))}
-            </div>
-          )}
-        </div>
+    <div className="border-space-blue bg-midnight-navy hover:border-aqua-mint mx-auto flex flex-col gap-6 rounded-lg border p-8 shadow-xl transition-colors duration-300">
+      <h2 className="text-center text-3xl leading-tight font-bold tracking-tight text-white">
+        {title}
+      </h2>
+      <div className="flex flex-col items-center gap-8 text-center">
+        <p className="text-lavender-blue max-w-2xl text-lg leading-relaxed font-normal whitespace-break-spaces">
+          {description}
+        </p>
+        {buttons && (
+          <div className="flex w-full flex-col gap-6 md:flex-row md:items-center md:justify-center md:gap-8">
+            {buttons.map((button) => (
+              <ButtonLink {...button} />
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
