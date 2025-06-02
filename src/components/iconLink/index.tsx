c682e93770f6280bc7ae858dd3c88887ecdbad0f
André Pacheco <andre.m.pacheco.97@gmail.com>
2025-06-02 20:19:32 +0100

    feat: created IconLink component

---
+++ src/components/iconLink/index.tsx
@@ -0,0 +1,44 @@
+import { iconMap, TIconName } from "constants/icons";
+
+export type TIconLinkProps = {
+  /**
+   * The icon aria-label
+   * @requires
+   */
+  ariaLabel: string;
+  /**
+   * The icon href
+   * @requires
+   */
+  href: string;
+  /**
+   * The icon name
+   * @requires
+   */
+  iconName: TIconName;
+  /**
+   * The icon size
+   * @optional
+   * @default 32
+   */
+  size?: number;
+};
+
+export const IconLink: React.FC<TIconLinkProps> = ({
+  ariaLabel,
+  href,
+  iconName,
+  size = 32,
+}) => {
+  const Icon = iconMap[iconName];
+
+  return (
+    <a
+      aria-label={ariaLabel}
+      className="text-lavender-blue hover:text-aqua-mint transition-colors"
+      href={href}
+    >
+      <Icon size={size} />
+    </a>
+  );
+};
