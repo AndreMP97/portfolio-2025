d0e25ec9241c69e28fa26d6a6d49ff016f1202be
André Pacheco <andre.m.pacheco.97@gmail.com>
2025-09-07 18:00:45 +0100

    fix: fixed loader timer

--- src/hooks/useSetup.ts
+++ src/hooks/useSetup.ts
@@ -1,11 +1,13 @@
 import { useEffect } from "react";
 import { useLoadingStore } from "stores/loadingStore";
 
+const LOADING_TIMER = 1500;
+
 export const useSetup = () => {
   useEffect(() => {
     const { setIsLoading } = useLoadingStore.getState();
 
-    const timer = setTimeout(() => setIsLoading(false), 2000);
+    const timer = setTimeout(() => setIsLoading(false), LOADING_TIMER);
     return () => clearTimeout(timer);
   }, []);
 };
