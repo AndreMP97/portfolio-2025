5bdec74557c17d69c8b20aacaa79c2b633db27d8
André Pacheco <andre.m.pacheco.97@gmail.com>
2025-09-12 23:49:32 +0100

    chore: improve rsbuild config and github workflows (#36)

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,23 +1,39 @@
-import * as path from 'path';
+import * as path from "path";
 import { defineConfig } from "@rsbuild/core";
 import { pluginReact } from "@rsbuild/plugin-react";
-import { pluginSvgr } from '@rsbuild/plugin-svgr';
+import { pluginSvgr } from "@rsbuild/plugin-svgr";
 
 export default defineConfig({
   html: {
     favicon: "./src/assets/favicon.ico",
     meta: {
-      description: `André Pacheco's Portfolio`,
+      charset: { charset: "UTF-8" },
+      description: "André Pacheco's Portfolio",
+      keywords:
+        "André Pacheco, Portfolio, Software Engineer, Web Developer, Frontend Developer, JavaScript, TypeScript, React.js, React.Native, Node.js, HTML, CSS",
+      viewport: "width=device-width, initial-scale=1, viewport-fit=cover",
     },
     template: "./src/assets/index.html",
+    title: "André Pacheco - Portfolio",
   },
-  plugins: [
-    pluginReact(),
-    pluginSvgr(),
-  ],
+  performance: {
+    /* bundleAnalyze: {
+      analyzerMode: "static",
+      reportFilename: "bundle-report.html",
+    }, */
+    prefetch: {
+      type: "async-chunks",
+    },
+    preload: {
+      include: (filename) => /\.(woff2?|webp)$/i.test(filename),
+      type: "all-assets",
+    },
+    removeConsole: true,
+  },
+  plugins: [pluginReact(), pluginSvgr()],
   resolve: {
     alias: {
-      '@assets': path.resolve(__dirname, 'src/assets'),
+      "@assets": path.resolve(__dirname, "src/assets"),
     },
   },
 });
