9d661a13e1adfe3f77aa297ba0da1c087a19c660
André Pacheco <andre.m.pacheco.97@gmail.com>
2025-09-12 21:08:11 +0100

    chore: improve rsbuild config and github workflows

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,15 +1,33 @@
-import * as path from 'path';
+import * as path from "path";
 import { defineConfig } from "@rsbuild/core";
 import { pluginReact } from "@rsbuild/plugin-react";
-import { pluginSvgr } from '@rsbuild/plugin-svgr';
+import { pluginSvgr } from "@rsbuild/plugin-svgr";
 
 export default defineConfig({
   html: {
     favicon: "./src/assets/favicon.ico",
     meta: {
-      description: `André Pacheco's Portfolio`,
+      charset: { charset: "UTF-8" },
+      description: "André Pacheco's Portfolio",
+      keywords: "André Pacheco, Portfolio, Software Engineer, Web Developer, Frontend Developer, JavaScript, TypeScript, React.js, React.Native, Node.js, HTML, CSS",
+      viewport: "width=device-width, initial-scale=1, viewport-fit=cover",
     },
     template: "./src/assets/index.html",
+    title: "André Pacheco - Portfolio",
+  },
+  performance: {
+    /* bundleAnalyze: {
+      analyzerMode: "static",
+      reportFilename: "bundle-report.html",
+    }, */
+    prefetch: {
+      type: "async-chunks",
+    },
+    preload: {
+      include: (filename) => /\.(woff2?|webp)$/i.test(filename),
+      type: "all-assets",
+    },
+    removeConsole: true,
   },
   plugins: [
     pluginReact(),
@@ -17,7 +35,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@assets': path.resolve(__dirname, 'src/assets'),
+      "@assets": path.resolve(__dirname, "src/assets"),
     },
   },
 });
